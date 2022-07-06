a2fc8dad24ebcf4a9459a748f39e38885c1c3dcb
CandyManCriminal <CandyManCriminal@gmail.com>
2022-07-05 20:47:08 -0400

    poked it, should be good

--- Assets/Scripts/Gameplay/ProjectileController.cs
+++ Assets/Scripts/Gameplay/ProjectileController.cs
@@ -5,72 +5,72 @@ using UnityEngine;
 
 namespace PotatoCat.Gameplay
 {
-   public class ProjectileController : MonoBehaviour
-   {
-      public int SecondsUntilDeath = 3;
-      public float Velocity = 1.0f;
-      public float SpinVelocity = -500;
-      public AudioClip CollisionAudio;
-      public GameObject BoomEffectPrefab;
+    public class ProjectileController : MonoBehaviour
+    {
+        public int SecondsUntilDeath = 3;
+        public float Velocity = 1.0f;
+        public float SpinVelocity = -500;
+        public AudioClip CollisionAudio;
+        public GameObject BoomEffectPrefab;
 
-      public Direction FireDirection { get; set; } = Direction.Right;
+        public Direction FireDirection { get; set; } = Direction.Right;
 
-      public enum Direction
-      {
-         Left,
-         Right
-      }
+        public enum Direction
+        {
+            Left,
+            Right
+        }
 
-      private SpriteRenderer mSpriteRenderer;
+        private SpriteRenderer mSpriteRenderer;
 
-      public static float skProjectileScale = 0.2967267f;
+        public static float skProjectileScale = 0.2967267f;
 
-      // Start is called before the first frame update
-      protected void Start()
-      {
-         mSpriteRenderer = GetComponent<SpriteRenderer>();
-         if (mSpriteRenderer != null && FireDirection == Direction.Right)
-         {
-            mSpriteRenderer.flipX = true;
-         }
-      }
+        // Start is called before the first frame update
+        protected void Start()
+        {
+            mSpriteRenderer = GetComponent<SpriteRenderer>();
+            if (mSpriteRenderer != null && FireDirection == Direction.Right)
+            {
+                mSpriteRenderer.flipX = true;
+            }
+        }
 
-      protected void OnCollisionEnter2D(Collision2D collision)
-      {
-         bool isEnemy = collision.gameObject.tag.Equals("enemy", StringComparison.OrdinalIgnoreCase);
-         bool isCollisionTerrain = collision.gameObject.tag.Equals("levelcollision", StringComparison.OrdinalIgnoreCase);
+        protected void OnCollisionEnter2D(Collision2D collision)
+        {
+            bool isEnemy = collision.gameObject.tag.Equals("enemy", StringComparison.OrdinalIgnoreCase);
+            bool isCollisionTerrain = collision.gameObject.tag.Equals("levelcollision", StringComparison.OrdinalIgnoreCase);
 
-         if (isEnemy || isCollisionTerrain)
-         {
-            HandleCollisionEffects();
-         }
-      }
+            if (isEnemy || isCollisionTerrain)
+            {
+                HandleCollisionEffects();
+            }
+        }
 
-      protected void FixedUpdate()
-      {
-         float xPosition = transform.position.x;
-         if (FireDirection == Direction.Left)
-         {
-            xPosition += (Velocity * Time.deltaTime);
-         }
-         else
-         {
-            xPosition -= (Velocity * Time.deltaTime);
-         }
+        protected void FixedUpdate()
+        {
+            float xPosition = transform.position.x;
+            if (FireDirection == Direction.Left)
+            {
+                xPosition += (Velocity * Time.deltaTime);
+            }
+            else
+            {
+                xPosition -= (Velocity * Time.deltaTime);
+            }
 
-         transform.Rotate(Vector3.forward * SpinVelocity * Time.deltaTime);
-         transform.position = new Vector3(xPosition,
-                                          transform.position.y,
-                                          transform.position.z);
-      }
+            transform.Rotate(Vector3.forward * SpinVelocity * Time.deltaTime);
+            transform.position = new Vector3(xPosition,
+                                             transform.position.y,
+                                             transform.position.z);
+        }
 
-      private void HandleCollisionEffects()
-      {
-         GameObject boom = Instantiate(BoomEffectPrefab);
-         boom.transform.position = gameObject.transform.position;
+        private void HandleCollisionEffects()
+        {
+            GameObject boom = Instantiate(BoomEffectPrefab);
+            boom.transform.position = gameObject.transform.position;
 
-         GameSimulation.Instance.AudioSource.PlayOneShot(CollisionAudio);
-         Destroy(gameObject);
-      }
-   }
+            GameSimulation.Instance.AudioSource.PlayOneShot(CollisionAudio);
+            Destroy(gameObject);
+        }
+    }
 }
